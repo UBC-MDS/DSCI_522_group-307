438d657207f18b2409c16bddf455f0f0cafed255
saurav99 <saurav13iitr@gmail.com>
2020-01-17 01:45:33 -0800

    added download script and eda

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,1 +1,21 @@
 # Author: Saurav Chowdhury
+# Date: 2020-16-01
+#
+# This script downloads all the required datasets and files and store them in desired location.
+# This script takes no arguments.
+#
+# Usage: Python download_data.py
+
+import urllib.request
+
+print('Beginning file download with urllib2...')
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+urllib.request.urlretrieve(url, '../data/adult_train.csv')
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test'
+urllib.request.urlretrieve(url, '../data/adult_test.csv')
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names'
+urllib.request.urlretrieve(url, '../data/adult_info.txt')
+
